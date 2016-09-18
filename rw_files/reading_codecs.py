d19befb331d52ea1e0b9ee05dc760c7674b0c169
rodrigorozendobastos@gmail.com <@georgerrmartin7>
2016-09-18 12:48:34 -0300

    reading file by codecs

---
+++ rw_files/reading_codecs.py
@@ -0,0 +1,18 @@
+# Example reading file by codecs
+
+# Encoders and Decoders
+import codecs
+
+# Open File by codecs
+# params: directory file, mode, encode
+# return object
+file = codecs.open("./rw_files/test_file.txt", "r", encoding="utf-8")
+print file
+
+# using read function to read all lines
+# return string
+# print file.read()
+
+# using readlines function to read all lines
+# return array
+print file.readlines()
